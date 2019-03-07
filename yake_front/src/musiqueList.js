1de8d96fa8b6b9e37a58c01ff4f9bc78132e2ed2
Baptiste Gallais <cormeray@live.fr>
2019-03-07 17:38:01 +0100

    micro modifs

--- yake_front/src/musiqueList.js
+++ yake_front/src/musiqueList.js
@@ -1,21 +1,19 @@
 import React, { Component } from 'react';
 import './MainContent.css';
-import { Button } from 'reactstrap';
 import AlbumsBlock from './AlbumsBlock.js';
 import ArtistesBlock from './ArtistesBlock.js';
 import MusiquesBlock from './MusiquesBlock.js';
 class musiqueList extends Component{
-    constructor(props){
-        super(props);
-    }
     render(){
+        //sortir le titre de musiquesBlock AlbumsBlock et ArtistesBlock 
+        //afin que chaque personne utilisant les blocks puissent les gérer comme elles veulent
         return <div className="MainContent">
                     <div className="scrollable">
-                <MusiquesBlock musiques={this.props.musiqueList} playlist={this.props.playlist} refresh={this.props.refresh}/>
-                <AlbumsBlock albums={this.props.albumList} onAlbumClick={this.props.onAlbumClick}/>
-                <ArtistesBlock artistes={this.props.artisteList} onArtisteClick={this.props.onArtisteClick}/>
+                        <MusiquesBlock musiques={this.props.musiqueList} playlist={this.props.playlist} refresh={this.props.refresh}/>
+                        <AlbumsBlock albums={this.props.albumList} onAlbumClick={this.props.onAlbumClick}/>
+                        <ArtistesBlock artistes={this.props.artisteList} onArtisteClick={this.props.onArtisteClick}/>
+                    </div>
                 </div>
-        </div>
     }
 }
 export default musiqueList;
